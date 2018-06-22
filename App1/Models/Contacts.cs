37d171516fd588b6951c451be672499f5dfc0888
Tirth Patel <tirth.patel@gisllc.com>
2018-06-22 01:28:28 -0400

    Add project files.

---
+++ App1/Models/Contacts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace App1.Models
+{
+    public class Contacts
+    {
+        public int ID { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public int address_number { get; set; }
+        public string street_name { get; set; }
+        public string street_type { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zip_code { get; set; }
+        public int phone_number { get; set; }
+    }
+}
---
+++ App1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(App1.Startup))]
+namespace App1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
